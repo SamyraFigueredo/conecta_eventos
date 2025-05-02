b5d95bf89f2b6dcee054d36974bdd62c5543f929
SamyraFigueredo <samyrafigueiredo24@gmail.com>
2025-05-02 10:50:02 -0300

    intalando dependencias JWT, e configurando para ambiente

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-module.exports = {
-    development: {
+const sequelize = new Sequelize({
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'mysql',
-}
-};
+});
+
+module.exports = sequelize;
