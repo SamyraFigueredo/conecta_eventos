fcb441d62e64efa81d6a45e2e559cb922afb8bfd
SamyraFigueredo <samyrafigueiredo24@gmail.com>
2025-05-05 10:36:55 -0300

    configurando ambiente para Testes

--- config/config.js
+++ config/config.js
@@ -1,5 +1,8 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
+const dotenvFlow = require('dotenv-flow');
+
+// Carrega .env ou .env.test automaticamente com base no NODE_ENV
+dotenvFlow.config();
 
 const sequelize = new Sequelize({
     host: process.env.DB_HOST,
@@ -7,6 +10,8 @@ const sequelize = new Sequelize({
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
+    logging: process.env.NODE_ENV === 'test' ? false : console.log, // silencia logs em testes
 });
 
 module.exports = sequelize;
