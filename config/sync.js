3687677017d1143b6616f1ccda9af116dc91fd67
SamyraFigueredo <samyrafigueiredo24@gmail.com>
2025-05-06 09:06:56 -0300

    criando sync.js para testar e sincronizar com bando de dados com modelos sequelize

---
+++ config/sync.js
@@ -0,0 +1,15 @@
+const sequelize = require('../Config/database_config');
+const Usuario = require('../Models/Usuario');
+
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('✅ Conexão com o banco de dados estabelecida com sucesso.');
+        await sequelize.sync({ alter: true });
+        console.log('✅ Tabela USUARIO sincronizada com sucesso.');
+    } catch (error) {
+        console.error('❌ Erro ao conectar ou sincronizar:', error);
+    } finally {
+        await sequelize.close();
+    }
+})();
