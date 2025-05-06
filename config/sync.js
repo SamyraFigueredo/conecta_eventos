58bb27cf369ef2e084db421432d3d8bc660b6855
SamyraFigueredo <samyrafigueiredo24@gmail.com>
2025-05-06 10:12:26 -0300

    generalizando importacao para todos os arquivos da pasta Model

--- config/sync.js
+++ config/sync.js
@@ -1,16 +1,26 @@
 const sequelize = require('../Config/database_config');
-const Usuario = require('../Models/Usuario');
+
+const fs = require('fs');
+const path = require('path');
+
+const modelsPath = path.join(__dirname, '..', 'Models');
+
+fs.readdirSync(modelsPath)
+.filter(file => file.endsWith('.js'))
+.forEach(file => {
+    require(path.join(modelsPath, file));
+});
 
 (async () => {
     try {
-        await sequelize.authenticate();
-        console.log('✅ Conexão com o banco de dados estabelecida com sucesso.');
+    await sequelize.authenticate();
+    console.log('✅ Conexão com o banco de dados estabelecida com sucesso.');
 
-        await sequelize.sync({ alter: true });
-        console.log('✅ Tabela USUARIO sincronizada com sucesso.');
-    } catch (error) {
-        console.error('❌ Erro ao conectar ou sincronizar:', error);
-    } finally {
-        await sequelize.close();
-    }
+    await sequelize.sync({ alter: true });
+    console.log('✅ Todas as tabelas foram sincronizadas com sucesso.');
+} catch (error) {
+    console.error('❌ Erro ao conectar ou sincronizar:', error);
+} finally {
+    await sequelize.close();
+}
 })();
