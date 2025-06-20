0677d1a0991b3da0d6e7bd97d4a7e9ec3187f230
SamyraFigueredo <samyrafigueiredo24@gmail.com>
2025-06-20 01:47:31 -0300

    adicionando rotas renderizadas do views

--- app.js
+++ app.js
@@ -26,6 +26,35 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/cadastro', (req, res) => {
+    res.render('cadastro');
+});
+
+app.get('/recuperar', (req, res) => {
+    res.render('recuperar'); // recuperar.ejs deve estar na pasta views/
+});
+
+app.get('/landingpaje_eventos', (_req, res) => {
+    res.render('landingpaje_eventos');
+});
+
+app.get('/landingpaje', (_req, res) => {
+    res.render('landingpaje');
+});
+
+app.get('/eventos_inscritos', (_req, res) => {
+    res.render('leventos_inscritos');
+});
+
+app.get('/emitir_certificados', (_req, res) => {
+    res.render('emitir_certificados');
+});
+
+
 const PORT = process.env.PORT || 3000;
 
 sequelize.sync({ force: false })
