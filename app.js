a1bd30140a17f3a286b029514d539823924a125f
SamyraFigueredo <samyrafigueiredo24@gmail.com>
2025-06-20 22:01:24 -0300

    adicionando tela de cadastro e login, com rotas e logica de servide e controller

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src', 'views'));
 
-app.use(express.static(path.join(__dirname, 'src', 'public')));
+app.use(express.static(path.join(__dirname, 'src/public')));
 
 // Rotas
 const usuarioRoutes = require('./src/routes/usuarioRoutes.js');
@@ -30,8 +30,8 @@ app.get('/auth/login', (req, res) => {
     res.render('login');
 });
 
-app.get('/cadastro', (req, res) => {
-    res.render('cadastro');
+app.get('/usuarios', (req, res) => {
+    res.render('usuario');
 });
 
 app.get('/recuperar', (req, res) => {
