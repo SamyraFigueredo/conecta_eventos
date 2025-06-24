d0f69e27e15a716429e5ba87907ef8378bc83eeb
SamyraFigueredo <samyrafigueiredo24@gmail.com>
2025-06-24 19:23:05 -0300

    feat: adiciona rotas de Programacao conectadas ao controller

---
+++ src/routes/programacaoRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const ProgramacaoController = require('../controllers/programacao.controller');
+
+router.get('/', ProgramacaoController.listar);
+router.get('/:id', ProgramacaoController.buscarPorId);
+router.post('/', ProgramacaoController.criar);
+router.put('/:id', ProgramacaoController.atualizar);
+router.delete('/:id', ProgramacaoController.deletar);
+
+module.exports = router;
