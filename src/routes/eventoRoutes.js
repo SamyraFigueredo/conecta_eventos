3bc8ceda4cac879168ad9f5814d72db470f247f3
SamyraFigueredo <samyrafigueiredo24@gmail.com>
2025-06-25 13:45:39 -0300

    criando nova rota para poder criar eventos no site, e listar eventos criados tambem

--- src/routes/eventoRoutes.js
+++ src/routes/eventoRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const EventoController = require('../controllers/eventoController');
 
-router.post('/', EventoController.criar);
-router.get('/', EventoController.listar);
+router.get('/', EventoController.listarView);
+router.post('/criarEvento', EventoController.criar);
 router.get('/:id', EventoController.buscarPorId);
 router.put('/:id', EventoController.atualizar);
 router.delete('/:id', EventoController.deletar);
